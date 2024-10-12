c8d9e0e3280a0f80367c6dc02ed92d7cce437baf
nshaw928 <nshaw928@gmail.com>
2024-10-12 01:47:30 -0400

    finished functionality of parser

--- src/parser.py
+++ src/parser.py
@@ -4,15 +4,24 @@ from parse_functions.parse_inline import parse_inline
 
 def parse(md_path):
     '''
-    Parses md file and outputs matching html file
+    Parses directory of md file and outputs list of parsed html body content to be added to an html template.
 
     Args:
         md_path (str): Path to folder storing md files
 
     Output:
-        website_page_content_list (list): A list where each element is a list containing each line of the webpage body as an element.
+        website (list): Each element is a page on the website. Within each page, each element is a line. The name of the page is the last element in the page list.
     '''
+    website = []
+
     for file in listdir(md_path):
+        
         file_path = md_path + '/' + file
-        print(file)
-        print(parse_inline(parse_structure(file_path)))
+        website_page_content_list = parse_inline(parse_structure(file_path))
+
+        file_name = file.split('.')[0]
+        website_page_content_list.append(file_name)
+
+        website.append(website_page_content_list)
+
+    return website
